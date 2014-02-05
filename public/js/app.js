f24ca0413e802ee93c8b048f5f075c8b553b3308
Emil <emilparikh@gmail.com>
2014-02-05 11:58:25 -0500

    try 3 to commit

--- public/js/app.js
+++ public/js/app.js
@@ -11,12 +11,33 @@ $('.each_tweet').draggable({
 
 });
 
+//makes accounts droppable
 $('.each_account').droppable({
     greedy: true,
     tolerance: 'touch',
     drop: function(event,ui){
         //test to detect overlap
-        alert('touched');
+        //alert(event.target.id);
+	//alert($(ui.draggable).attr("id"));
+	var lastAcct = event.target.id.substr(event.target.id.length - 1);
+	var lastTwt = $(ui.draggable).attr("id").substr($(ui.draggable).attr("id").length  - 1);
+
+	if(lastAcct == lastTwt)
+		alert("Correct!");
+	else
+		alert("Try again.");
+	
     }
 });
+}
+
+//body onload, shuffles the list items
+function shuffle(){
+	var olA = document.getElementById("accounts");
+	var olT = document.getElementById("tweets");
+
+	for (var i = olA.children.length; i >= 0; i--){
+    		olA.appendChild(olA.children[Math.random() * i | 0]);
+		olT.appendChild(olT.children[Math.random() * i | 0]);
+	}
 }
